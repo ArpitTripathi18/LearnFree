4272ba1eac1e61d087d5617d8e1bddfdf4133aef
Arpit Tripathi <60655613+ArpitTripathi18@users.noreply.github.com>
2021-10-28 17:00:33 +0530

    Update BFS_Algo.c

--- C-lang Codes/BFS_Algo.c
+++ C-lang Codes/BFS_Algo.c
@@ -1,167 +1,137 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 40
+#include <stdbool.h>
 
-struct queue {
-  int items[SIZE];
-  int front;
-  int rear;
+#define MAX 5
+
+struct Vertex {
+   char label;
+   bool visited;
 };
 
-struct queue* createQueue();
-void enqueue(struct queue* q, int);
-int dequeue(struct queue* q);
-void display(struct queue* q);
-int isEmpty(struct queue* q);
-void printQueue(struct queue* q);
+//queue variables
 
-struct node {
-  int vertex;
-  struct node* next;
-};
+int queue[MAX];
+int rear = -1;
+int front = 0;
+int queueItemCount = 0;
 
-struct node* createNode(int);
+//graph variables
 
-struct Graph {
-  int numVertices;
-  struct node** adjLists;
-  int* visited;
-};
+//array of vertices
+struct Vertex* lstVertices[MAX];
 
-// BFS algorithm
-void bfs(struct Graph* graph, int startVertex) {
-  struct queue* q = createQueue();
+//adjacency matrix
+int adjMatrix[MAX][MAX];
 
-  graph->visited[startVertex] = 1;
-  enqueue(q, startVertex);
+//vertex count
+int vertexCount = 0;
 
-  while (!isEmpty(q)) {
-    printQueue(q);
-    int currentVertex = dequeue(q);
-    printf("Visited %d\n", currentVertex);
+//queue functions
 
-    struct node* temp = graph->adjLists[currentVertex];
+void insert(int data) {
+   queue[++rear] = data;
+   queueItemCount++;
+}
 
-    while (temp) {
-      int adjVertex = temp->vertex;
+int removeData() {
+   queueItemCount--;
+   return queue[front++]; 
+}
 
-      if (graph->visited[adjVertex] == 0) {
-        graph->visited[adjVertex] = 1;
-        enqueue(q, adjVertex);
+bool isQueueEmpty() {
+   return queueItemCount == 0;
+}
+
+//graph functions
+
+//add vertex to the vertex list
+void addVertex(char label) {
+   struct Vertex* vertex = (struct Vertex*) malloc(sizeof(struct Vertex));
+   vertex->label = label;  
+   vertex->visited = false;     
+   lstVertices[vertexCount++] = vertex;
+}
+
+//add edge to edge array
+void addEdge(int start,int end) {
+   adjMatrix[start][end] = 1;
+   adjMatrix[end][start] = 1;
+}
+
+//display the vertex
+void displayVertex(int vertexIndex) {
+   printf("%c ",lstVertices[vertexIndex]->label);
+}       
+
+//get the adjacent unvisited vertex
+int getAdjUnvisitedVertex(int vertexIndex) {
+   int i;
+	
+   for(i = 0; i<vertexCount; i++) {
+      if(adjMatrix[vertexIndex][i] == 1 && lstVertices[i]->visited == false)
+         return i;
+   }
+	
+   return -1;
+}
+
+void breadthFirstSearch() {
+   int i;
+
+   //mark first node as visited
+   lstVertices[0]->visited = true;
+
+   //display the vertex
+   displayVertex(0);   
+
+   //insert vertex index in queue
+   insert(0);
+   int unvisitedVertex;
+
+   while(!isQueueEmpty()) {
+      //get the unvisited vertex of vertex which is at front of the queue
+      int tempVertex = removeData();   
+
+      //no adjacent vertex found
+      while((unvisitedVertex = getAdjUnvisitedVertex(tempVertex)) != -1) {    
+         lstVertices[unvisitedVertex]->visited = true;
+         displayVertex(unvisitedVertex);
+         insert(unvisitedVertex);               
       }
-      temp = temp->next;
-    }
-  }
-}
+		
+   }   
 
-// Creating a node
-struct node* createNode(int v) {
-  struct node* newNode = malloc(sizeof(struct node));
-  newNode->vertex = v;
-  newNode->next = NULL;
-  return newNode;
-}
-
-// Creating a graph
-struct Graph* createGraph(int vertices) {
-  struct Graph* graph = malloc(sizeof(struct Graph));
-  graph->numVertices = vertices;
-
-  graph->adjLists = malloc(vertices * sizeof(struct node*));
-  graph->visited = malloc(vertices * sizeof(int));
-
-  int i;
-  for (i = 0; i < vertices; i++) {
-    graph->adjLists[i] = NULL;
-    graph->visited[i] = 0;
-  }
-
-  return graph;
-}
-
-// Add edge
-void addEdge(struct Graph* graph, int src, int dest) {
-  // Add edge from src to dest
-  struct node* newNode = createNode(dest);
-  newNode->next = graph->adjLists[src];
-  graph->adjLists[src] = newNode;
-
-  // Add edge from dest to src
-  newNode = createNode(src);
-  newNode->next = graph->adjLists[dest];
-  graph->adjLists[dest] = newNode;
-}
-
-// Create a queue
-struct queue* createQueue() {
-  struct queue* q = malloc(sizeof(struct queue));
-  q->front = -1;
-  q->rear = -1;
-  return q;
-}
-
-// Check if the queue is empty
-int isEmpty(struct queue* q) {
-  if (q->rear == -1)
-    return 1;
-  else
-    return 0;
-}
-
-// Adding elements into queue
-void enqueue(struct queue* q, int value) {
-  if (q->rear == SIZE - 1)
-    printf("\nQueue is Full!!");
-  else {
-    if (q->front == -1)
-      q->front = 0;
-    q->rear++;
-    q->items[q->rear] = value;
-  }
-}
-
-// Removing elements from queue
-int dequeue(struct queue* q) {
-  int item;
-  if (isEmpty(q)) {
-    printf("Queue is empty");
-    item = -1;
-  } else {
-    item = q->items[q->front];
-    q->front++;
-    if (q->front > q->rear) {
-      printf("Resetting queue ");
-      q->front = q->rear = -1;
-    }
-  }
-  return item;
-}
-
-// Print the queue
-void printQueue(struct queue* q) {
-  int i = q->front;
-
-  if (isEmpty(q)) {
-    printf("Queue is empty");
-  } else {
-    printf("\nQueue contains \n");
-    for (i = q->front; i < q->rear + 1; i++) {
-      printf("%d ", q->items[i]);
-    }
-  }
+   //queue is empty, search is complete, reset the visited flag        
+   for(i = 0;i<vertexCount;i++) {
+      lstVertices[i]->visited = false;
+   }    
 }
 
 int main() {
-  struct Graph* graph = createGraph(6);
-  addEdge(graph, 0, 1);
-  addEdge(graph, 0, 2);
-  addEdge(graph, 1, 2);
-  addEdge(graph, 1, 4);
-  addEdge(graph, 1, 3);
-  addEdge(graph, 2, 4);
-  addEdge(graph, 3, 4);
+   int i, j;
 
-  bfs(graph, 0);
+   for(i = 0; i<MAX; i++) // set adjacency {
+      for(j = 0; j<MAX; j++) // matrix to 0
+         adjMatrix[i][j] = 0;
+   }
 
-  return 0;
+   addVertex('S');   // 0
+   addVertex('A');   // 1
+   addVertex('B');   // 2
+   addVertex('C');   // 3
+   addVertex('D');   // 4
+ 
+   addEdge(0, 1);    // S - A
+   addEdge(0, 2);    // S - B
+   addEdge(0, 3);    // S - C
+   addEdge(1, 4);    // A - D
+   addEdge(2, 4);    // B - D
+   addEdge(3, 4);    // C - D
+	
+   printf("\nBreadth First Search: ");
+   
+   breadthFirstSearch();
+
+   return 0;
 }
